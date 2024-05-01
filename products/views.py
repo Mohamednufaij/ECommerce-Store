d3f7e2079d083e86ebb3ca1e9bef9a1bec86f20a
mohamed nufaij <nufaijjufu321@gmail.com>
2024-05-01 11:08:33 +0530

    solved few error on products page

--- products/views.py
+++ products/views.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from . models import Product
 from django.core.paginator import Paginator
 # Create your views here.
+
+
 def index(request):
     featured_products=Product.objects.order_by('priority')[:3]
     latest_products=Product.objects.order_by('-id')[:3]
@@ -12,6 +14,8 @@ def index(request):
     }
     
     return render(request,'index.html',context)
+
+
 def products(request):
     page=1
     if request.GET:
@@ -19,11 +23,19 @@ def products(request):
     product_list=Product.objects.order_by('priority')
     product_paginator=Paginator(product_list,2)
     product_list=product_paginator.get_page(page)
+    # product_ids = [product for product in product_list]
+    
     context={'products':product_list}
+    # for product in product_ids:
+    #     print("*"*50)
+    #     print(product.id)
+    #     print("*"*50)
     return render (request,'products.html',context)
+
+
 def product_details(request,id):
     product=Product.objects.get(id=id)
     context={'product':product}
-    # return render(request,'product_details.html',context)
-    product_id = 1
-    return redirect(reverse('product_details', kwargs={'id': product_id}))
+    return render(request,'product_details.html',context)
+    # product_id = 1
+    # return redirect('product_details', kwargs={'id': product_id}))
